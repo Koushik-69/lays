a5f8b776d494a8e1708fc87562e6e3eeb083f93c
Koushik-69 <136176909+Koushik-69@users.noreply.github.com>
2024-11-06 23:43:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 const Product = require("./models/product.js");
 const port = process.env.PORT || 3000;
-//adding mongo db
+
+// MongoDB connection
 main().then((res) => {
-  console.log("connect to db");
+  console.log("Connected to DB");
 })
-  .catch((err) => {
-    console.log(err);
-  })
+.catch((err) => {
+  console.log(err);
+});
+
 async function main() {
   const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/crunchyHub';
   await mongoose.connect(dbURI, {
@@ -26,44 +28,39 @@ async function main() {
   });
 }
 
+// Start server
 app.listen(port, () => {
-  console.log("app ");
+  console.log("App running on port", port);
+});
 
-})
-
+// Route Handlers
 app.get('/', (req, res) => {
   res.render("layouts/front.ejs");
-
-})
+});
 
 app.post('/home', async (req, res) => {
   const data = req.body;
   data.txt = data.txt.toUpperCase();
- const products = await Product.find({});
- res.render("layouts/home.ejs", { data, products });
+  const products = await Product.find({});
+  res.render("layouts/home.ejs", { data, products });
+});
 
-})
 app.get('/home', async (req, res) => {
-
- const products = await Product.find({});
- res.render("layouts/home.ejs", {  products });
-
-})
-
+  const products = await Product.find({});
+  res.render("layouts/home.ejs", { products });
+});
 
 app.get('/cart', async (req, res) => {
   const products = await Product.find({});
-
   res.render("layouts/cart.ejs", { products });
-})
+});
 
-app.get('/show/:id',async(req,res)=>{
-  const {id}=req.params;
+app.get('/show/:id', async (req, res) => {
+  const { id } = req.params;
   const products = await Product.findById(id);
-  
-  res.render("layouts/show.ejs",{products});
-})
+  res.render("layouts/show.ejs", { products });
+});
 
-app.get("/about_us",(req,res)=>{
+app.get('/about_us', (req, res) => { // Corrected route for about_us
   res.render("layouts/about_us.ejs");
-})
+});
